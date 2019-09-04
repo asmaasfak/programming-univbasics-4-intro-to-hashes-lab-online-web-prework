7efcaf0ef405299f34d1f46e7e49afa8673c801a
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-04 02:57:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,10 +10,12 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = { :name => "Grace Hopper"}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  id_gen = { :id => 123 }
 end
 
 def my_hash_creator(key, value)
